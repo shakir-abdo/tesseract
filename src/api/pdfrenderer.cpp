592f50ccca0519d9ba644b148582a1c4890233f8
Stefan Weil <sw@weilnetz.de>
2018-07-03 19:52:16 +0200

    pdfrenderer w.i.p.

--- src/api/pdfrenderer.cpp
+++ src/api/pdfrenderer.cpp
@@ -229,27 +229,23 @@ static void GetWordBaseline(int writing_direction, int ppi, int height,
     Swap(&word_x1, &word_x2);
     Swap(&word_y1, &word_y2);
   }
-  double word_length;
+  int px = word_x1;
+  int py = word_y1;
+  double l2 = dist2(line_x1, line_y1, line_x2, line_y2);
   double x, y;
-  {
-    int px = word_x1;
-    int py = word_y1;
-    double l2 = dist2(line_x1, line_y1, line_x2, line_y2);
-    if (l2 == 0) {
-      x = line_x1;
-      y = line_y1;
-    } else {
-      double t = ((px - line_x2) * (line_x2 - line_x1) +
-                  (py - line_y2) * (line_y2 - line_y1)) / l2;
-      x = line_x2 + t * (line_x2 - line_x1);
-      y = line_y2 + t * (line_y2 - line_y1);
-    }
-    word_length = sqrt(static_cast<double>(dist2(word_x1, word_y1,
-                                                 word_x2, word_y2)));
-    word_length = word_length * 72.0 / ppi;
-    x = x * 72 / ppi;
-    y = height - (y * 72.0 / ppi);
+  if (l2 == 0) {
+    x = line_x1;
+    y = line_y1;
+  } else {
+    double t = ((px - line_x2) * (line_x2 - line_x1) +
+                (py - line_y2) * (line_y2 - line_y1)) / l2;
+    x = line_x2 + t * (line_x2 - line_x1);
+    y = line_y2 + t * (line_y2 - line_y1);
   }
+  double word_length = sqrt(dist2(word_x1, word_y1, word_x2, word_y2));
+  word_length = word_length * 72.0 / ppi;
+  x = x * 72 / ppi;
+  y = height - (y * 72.0 / ppi);
   *x0 = x;
   *y0 = y;
   *length = word_length;
